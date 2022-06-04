54696000829e101daf36b1e93807e73d15f7cb3e
Sunny <sunny.huang@cloudlytics.me>
2022-06-04 22:51:22 +0800

    add member page

--- src/pages/Member/index.tsx
+++ src/pages/Member/index.tsx
@@ -1,9 +1,20 @@
+import styled from "./Member.module.scss"
+import Button from "components/Button"
+import Profile from "components/Profile"
 import BottomNavigation from "components/BottomNavigation"
 
 const Member = () => {
   return (
     <>
-      <div>Member</div>
+      <Profile />
+      <div className={styled.wrapper}>
+        <Button variant="secondary">諮詢歷史紀錄</Button>
+        <Button variant="secondary">收藏案例</Button>
+        <Button variant="secondary">修改密碼</Button>
+        <Button variant="secondary">常見問答</Button>
+        <div />
+        <Button variant="text">登出</Button>
+      </div>
       <BottomNavigation />
     </>
   )

8a062f8eeb338fea0aa6dd5ccb8fbd447f923167
Sunny <sunny.huang@cloudlytics.me>
2022-06-04 23:04:13 +0800

    add doctor information page

--- src/pages/DoctorInformation/index.tsx
+++ src/pages/DoctorInformation/index.tsx
@@ -1,4 +1,20 @@
+import styled from "./DoctorInformation.module.scss"
+import Button from "components/Button"
+import Profile from "components/Profile"
+import BottomNavigation from "components/BottomNavigation"
+
 const DoctorInformation = () => {
-  return <div>Doctor Information</div>
+  return (
+    <>
+      <Profile />
+      <div className={styled.wrapper}>
+        <Button variant="secondary">專長項目</Button>
+        <Button variant="secondary">修改密碼</Button>
+        <div />
+        <Button variant="text">登出</Button>
+      </div>
+      <BottomNavigation.Chat />
+    </>
+  )
 }
 export default DoctorInformation

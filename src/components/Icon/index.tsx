99ad9434beca2ca652a1e1db268b01cb4304f6fe
Sunny <sunny.huang@cloudlytics.me>
2022-06-04 22:08:19 +0800

    add the icon component

---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import Svg from "./svg"
+import cx from "classnames"
+import styled from "./Icon.module.scss"
+
+export type IconProps = {
+  /**
+   * Fill in the icon name.
+   */
+  name: string
+  width?: number
+  height?: number
+} & ReactProps.WithClassName &
+  ReactProps.WithStyle
+
+export const Icon = (props: IconProps) => {
+  const camelCased = props.name.replace(/-([a-z])/g, g => g[1].toUpperCase())
+  const compName = camelCased.charAt(0).toUpperCase() + camelCased.slice(1)
+
+  const compareIcon: { [key: string]: JSX.Element } = {
+    EyeSlash: <Svg.EyeSlash />,
+    EyeFill: <Svg.EyeFill />,
+    Check: <Svg.Check />,
+    Cross: <Svg.Cross />,
+  }
+
+  return (
+    <div
+      className={cx({ [styled.arrowWrapper]: compName.includes("Arrow") }, props.className)}
+      style={{ display: "flex", ...props.style }}>
+      {React.cloneElement(compareIcon[compName], {
+        ...(props.width && { width: props.width }),
+        ...(props.height && { height: props.height }),
+      })}
+    </div>
+  )
+}
+
+export default Icon
---
+++ src/components/Icon/svg/index.ts
@@ -0,0 +1,16 @@
+import { ReactComponent as EyeSlash } from "./EyeSlash.svg"
+import { ReactComponent as EyeFill } from "./EyeFill.svg"
+import { ReactComponent as Check } from "./Check.svg"
+import { ReactComponent as Cross } from "./Cross.svg"
+
+const Image = Object.assign(
+  {},
+  {
+    EyeSlash,
+    EyeFill,
+    Check,
+    Cross,
+  },
+)
+
+export default Image

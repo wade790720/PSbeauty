8f263d364ffb0d4557b16301661bc978b72f405a
Sunny <sunny.huang@cloudlytics.me>
2022-06-05 18:21:26 +0800

    modify header component

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,12 +1,31 @@
 import styled from "./Header.module.scss"
 import cx from "classnames"
+import Icon from "components/Icon"
 
-export type HeaderProps = ReactProps.Component
+export type HeaderProps = {
+  leftArrow?: boolean
+  title?: string
+} & ReactProps.Component
 
 const Header = ({ style = {}, ...props }: HeaderProps) => {
   return (
-    <div className={cx(styled.wrapper, props.className)} style={{ ...style }}>
-      {props.children}
+    <div
+      className={cx(styled.wrapper, props.className, {
+        [styled.large]: props.title,
+        [styled.small]: !props.title && !props.leftArrow,
+      })}
+      style={{ ...style }}>
+      {props.leftArrow && <Icon className={styled["back"]} name="LeftArrow" />}
+      {props.title ? (
+        <div
+          className={cx(styled.title, {
+            [styled["has-back"]]: props.leftArrow,
+          })}>
+          {props.title}
+        </div>
+      ) : (
+        props.children
+      )}
     </div>
   )
 }

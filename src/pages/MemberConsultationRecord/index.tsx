661fa844d1ac684ff84cba9c86cc0b246e602a92
Sunny <sunny.huang@cloudlytics.me>
2022-06-05 22:00:15 +0800

    add member consultation record page

--- src/pages/MemberConsultationRecord/index.tsx
+++ src/pages/MemberConsultationRecord/index.tsx
@@ -1,4 +1,26 @@
+import styled from "./MemberConsultationRecord.module.scss"
+import Header from "components/Layout/Header"
+import MessageCard from "components/MessageCard"
+
 const MemberConsultationRecord = () => {
-  return <div>Member Consultation Record</div>
+  return (
+    <div className={styled.wrapper}>
+      <Header title="收件夾" leftArrow />
+      <div className={styled.cards}>
+        <MessageCard
+          unread
+          title="玉辛醫美診所"
+          message="您好，我有「雙眼皮」「痘痘針」「玻尿..."
+        />
+        <MessageCard unread title="山茼醫美診所" message="好的，謝謝告知。" />
+        <MessageCard unread title="和黛醫美診所" message="了解，回診時間再跟您電話聯繫。" />
+        <MessageCard title="關楊哲健康醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
+        <MessageCard title="五代醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
+        <MessageCard title="健康醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
+        <MessageCard title="玉辛醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
+        <MessageCard title="山茼醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
+      </div>
+    </div>
+  )
 }
 export default MemberConsultationRecord

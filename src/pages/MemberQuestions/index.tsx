81571cb369e5e4c80f2debcc2451282284be564a
Sunny <sunny.huang@cloudlytics.me>
2022-06-05 18:22:04 +0800

    add member questions page

--- src/pages/MemberQuestions/index.tsx
+++ src/pages/MemberQuestions/index.tsx
@@ -1,4 +1,37 @@
+import styled from "./MemberQuestions.module.scss"
+import Backdrop from "components/Layout/Backdrop"
+import Header from "components/Layout/Header"
+
 const MemberQuestions = () => {
-  return <div>Member Questions</div>
+  return (
+    <div className={styled.wrapper}>
+      <Header title="常見問題" leftArrow />
+      <Backdrop>
+        <div className={styled.paragraph}>
+          <div>1. 可列舉常見問題與情境範例文字？</div>
+          <div>
+            A.
+            回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字。
+          </div>
+        </div>
+
+        <div className={styled.paragraph}>
+          <div>2. 可列舉常見問題與情境範例文字？</div>
+          <div>
+            A.
+            回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字。
+          </div>
+        </div>
+
+        <div className={styled.paragraph}>
+          <div>3. 可列舉常見問題與情境範例文字？</div>
+          <div>
+            A.
+            回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字，回答問題與情境範例文字。
+          </div>
+        </div>
+      </Backdrop>
+    </div>
+  )
 }
 export default MemberQuestions

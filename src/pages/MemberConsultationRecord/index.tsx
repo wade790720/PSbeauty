424e9fe8f26d9c4bfb9098e643f2a31a47fc96a0
Sunny <sunny.huang@cloudlytics.me>
2022-06-08 01:26:51 +0800

    add member consultation record page

--- src/pages/MemberConsultationRecord/index.tsx
+++ src/pages/MemberConsultationRecord/index.tsx
@@ -1,26 +1,49 @@
 import styled from "./MemberConsultationRecord.module.scss"
 import Header from "components/Layout/Header"
-import MessageCard from "components/MessageCard"
+import HistoryRecordCard from "components/HistoryRecordCard"
+import Left from "./Left.png"
+import Front from "./Front.png"
+import Right from "./Right.png"
 
 const MemberConsultationRecord = () => {
   return (
-    <div className={styled.wrapper}>
-      <Header title="收件夾" leftArrow />
-      <div className={styled.cards}>
-        <MessageCard
-          unread
-          title="玉辛醫美診所"
-          message="您好，我有「雙眼皮」「痘痘針」「玻尿..."
+    <>
+      <Header title="收諮詢歷史紀錄件夾" leftArrow />
+      <div className={styled.wrapper}>
+        <HistoryRecordCard
+          title="側臉線條"
+          date="2022-05-18｜剩餘3天"
+          toggle={true}
+          images={[Left, Front, Right]}
+          introduction="諮詢內容簡介說明文字，請將症例相關說明文字，僅供參考排版樣式，內容簡介...顯示更多"
+          tags={["蘋果肌", "痘痘針", "玻尿酸", "蘋果肌2", "痘痘針2", "玻尿酸2"]}
         />
-        <MessageCard unread title="山茼醫美診所" message="好的，謝謝告知。" />
-        <MessageCard unread title="和黛醫美診所" message="了解，回診時間再跟您電話聯繫。" />
-        <MessageCard title="關楊哲健康醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
-        <MessageCard title="五代醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
-        <MessageCard title="健康醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
-        <MessageCard title="玉辛醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
-        <MessageCard title="山茼醫美診所" message="您好，我有「雙眼皮」「痘痘針」「玻尿..." />
+        <HistoryRecordCard
+          title="側臉線條"
+          date="2022-05-18｜剩餘3天"
+          toggle={false}
+          images={[Left, Front, Right]}
+          introduction="諮詢內容簡介說明文字，請將症例相關說明文字，僅供參考排版樣式，內容簡介...顯示更多"
+          tags={["蘋果肌", "痘痘針", "玻尿酸", "蘋果肌2", "痘痘針2", "玻尿酸2"]}
+        />
+        <HistoryRecordCard
+          title="側臉線條"
+          date="2022-05-18｜剩餘3天"
+          toggle={false}
+          images={[Left, Front, Right]}
+          introduction="諮詢內容簡介說明文字，請將症例相關說明文字，僅供參考排版樣式，內容簡介...顯示更多"
+          tags={["蘋果肌", "痘痘針", "玻尿酸", "蘋果肌2", "痘痘針2", "玻尿酸2"]}
+        />
+        <HistoryRecordCard
+          title="側臉線條"
+          date="2022-05-18｜剩餘3天"
+          toggle={false}
+          images={[Left, Front, Right]}
+          introduction="諮詢內容簡介說明文字，請將症例相關說明文字，僅供參考排版樣式，內容簡介...顯示更多"
+          tags={["蘋果肌", "痘痘針", "玻尿酸", "蘋果肌2", "痘痘針2", "玻尿酸2"]}
+        />
       </div>
-    </div>
+    </>
   )
 }
 export default MemberConsultationRecord

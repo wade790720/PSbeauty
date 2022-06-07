7158d3007f1079ef0966226dc43ca3787c3b3445
Sunny <sunny.huang@cloudlytics.me>
2022-06-08 01:37:07 +0800

    top information in chatroom

--- src/components/HistoryRecordCard/index.tsx
+++ src/components/HistoryRecordCard/index.tsx
@@ -4,7 +4,7 @@ import Switch from "react-switch"
 export type HistoryRecordCardProps = {
   title: string
   date: string
-  toggle: boolean
+  toggle?: boolean
   images: string[]
   introduction: string
   tags?: string[]
@@ -29,21 +29,23 @@ const HistoryRecordCard = ({ ...props }: HistoryRecordCardProps) => {
           </div>
         ))}
       </div>
-      <div className={styled.toggle}>
-        <Switch
-          checkedIcon={false}
-          uncheckedIcon={false}
-          width={51}
-          height={31}
-          onColor="#7873E5"
-          offColor="#CBCBCD"
-          activeBoxShadow="0 0 2px 3px #7873E533"
-          checked={props.toggle}
-          onChange={() => {
-            console.log("toggle", props.toggle)
-          }}
-        />
-      </div>
+      {typeof props.toggle === "boolean" && (
+        <div className={styled.toggle}>
+          <Switch
+            checkedIcon={false}
+            uncheckedIcon={false}
+            width={51}
+            height={31}
+            onColor="#7873E5"
+            offColor="#CBCBCD"
+            activeBoxShadow="0 0 2px 3px #7873E533"
+            checked={props.toggle}
+            onChange={() => {
+              console.log("toggle", props.toggle)
+            }}
+          />
+        </div>
+      )}
     </div>
   )
 }
--- src/pages/Chatroom/index.tsx
+++ src/pages/Chatroom/index.tsx
@@ -2,7 +2,11 @@ import styled from "./Chatroom.module.scss"
 import cx from "classnames"
 import Header from "components/Layout/Header"
 import Backdrop from "components/Layout/Backdrop"
+import HistoryRecordCard from "components/HistoryRecordCard"
 import { ReactComponent as UploadImage } from "./UploadImage.svg"
+import Left from "./Left.png"
+import Front from "./Front.png"
+import Right from "./Right.png"
 
 const Chatroom = () => {
   return (
@@ -11,6 +15,15 @@ const Chatroom = () => {
       <Backdrop className={styled.wrapper}>
         <div className={cx(styled.row, styled.center)}>
           <div className={styled.time}>Yesterday 9:41</div>
+        </div>
+        <div className={cx(styled.row, styled.center)}>
+          <HistoryRecordCard
+            title="側臉線條"
+            date="2022-05-18｜剩餘3天"
+            images={[Left, Front, Right]}
+            introduction="諮詢內容簡介說明文字，請將症例相關說明文字，僅供參考排版樣式，內容簡介...顯示更多"
+            tags={["蘋果肌", "痘痘針", "玻尿酸", "蘋果肌2", "痘痘針2", "玻尿酸2"]}
+          />
         </div>
         <div className={cx(styled.row, styled.right)}>
           <div className={styled.message}>您好，我有「全臉」相關問題想諮詢王重陽醫師。</div>

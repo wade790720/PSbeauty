d217cec6ab322f42b44ab4586f0c1c0f01e32894
Sunny <sunny.huang@cloudlytics.me>
2022-06-09 22:57:58 +0800

    add the case card component to the home page

--- src/components/CaseCard/index.tsx
+++ src/components/CaseCard/index.tsx
@@ -1,17 +1,21 @@
 import styled from "./CaseCard.module.scss"
 import { ReactComponent as BookmarkFill } from "./BookmarkFill.svg"
+import { ReactComponent as BookmarkSimple } from "./BookmarkSimple.svg"
 
 export type CaseCardProps = {
   title: string
   clinic: string
   introduction: string
   images: string[]
+  isCollected: boolean
   tags?: string[]
+  amount?: string // 收藏人數
 } & ReactProps.Component
 
 const CaseCard = ({ ...props }: CaseCardProps) => {
+  console.log("### amount", props.amount)
   return (
-    <div className={styled.wrapper}>
+    <div className={styled.wrapper} style={props.style}>
       <div className={styled.title}>{props.title}</div>
       <div className={styled.clinic}>{props.clinic}</div>
       <div className={styled.content}>{props.introduction}</div>
@@ -28,7 +32,14 @@ const CaseCard = ({ ...props }: CaseCardProps) => {
           </div>
         ))}
       </div>
-      <BookmarkFill className={styled.bookmark} />
+      <div className={styled["collect-block"]}>
+        {props.isCollected ? (
+          <BookmarkFill className={styled["bookmark-fill"]} />
+        ) : (
+          <BookmarkSimple className={styled["bookmark-simple"]} />
+        )}
+        <div className={styled.amount}>{props.amount}</div>
+      </div>
     </div>
   )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,9 @@ import BottomNavigation from "components/BottomNavigation"
 import SearchBar from "components/SearchBar"
 import AdCard from "./AdCard"
 import Button from "components/Button"
+import CaseCard from "components/CaseCard"
+import imgBefore from "pages/MemberCollectClinicalCase/Before.png"
+import imgAfter from "pages/MemberCollectClinicalCase/After.png"
 
 const Home = () => {
   return (
@@ -18,6 +21,16 @@ const Home = () => {
           <div className={styled["banner-item"]}>banner-item</div>
         </div>
         <AdCard />
+        <CaseCard
+          style={{ marginBottom: "12px" }}
+          amount="1.2K"
+          isCollected={false}
+          title="臉部拉提改善面部線條A"
+          clinic="玉辛醫美診所"
+          introduction="網友熱門討論項目大都集中在面部雷射光療，以及肉毒桿菌醫學美容，解決不少有咀嚼肌的問題"
+          images={[imgBefore, imgAfter]}
+          tags={["蘋果肌1", "蘋果肌2", "蘋果肌3", "蘋果肌4", "蘋果肌5"]}
+        />
         <AdCard />
         <AdCard />
         <AdCard />

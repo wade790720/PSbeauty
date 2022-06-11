a77201821e815c0c802410c09d4e648701ca56d0
Pearl Yeh <pearl.yeh@cloudlytics.me>
2022-06-12 00:27:25 +0800

    add clinicActivities page and fix person svg size

---
+++ src/pages/Clinic/ClinicActivities/ActivityCard.tsx
@@ -0,0 +1,17 @@
+import styled from "./ClinicActivities.module.scss"
+
+const ActivityCard = () => {
+  return (
+    <div className={styled["activity-card"]}>
+      <div className={styled.title}>VIP會員方案-玻尿酸享半價優惠</div>
+      <div className={styled.content}>
+        <div className={styled.pic} />
+        <div className={styled.text}>
+          網友熱門討論項目大都集中在面部雷射光療，以及肉毒桿菌醫學美容，解決不少有咀嚼肌困擾的愛美女孩煩惱，讓臉...
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ActivityCard
--- src/pages/Clinic/ClinicActivities/index.tsx
+++ src/pages/Clinic/ClinicActivities/index.tsx
@@ -1,4 +1,14 @@
+import styled from "./ClinicActivities.module.scss"
+import ActivityCard from "./ActivityCard"
+
 const ClinicActivities = () => {
-  return <div>Clinic Activities</div>
+  return (
+    <div className={styled.wrapper}>
+      <ActivityCard />
+      <ActivityCard />
+      <ActivityCard />
+      <ActivityCard />
+    </div>
+  )
 }
 export default ClinicActivities

5459c166986a3a67ab8b114d30eebc081e291241
Pearl Yeh <pearl.yeh@cloudlytics.me>
2022-06-20 22:03:49 +0800

    fix doctor card show more function

--- src/pages/Clinic/ClinicMedicalTeam/DoctorCard.tsx
+++ src/pages/Clinic/ClinicMedicalTeam/DoctorCard.tsx
@@ -1,33 +1,61 @@
-import { useState } from "react"
+import { useState, useRef, useLayoutEffect } from "react"
 import styled from "./ClinicMedicalTeam.module.scss"
 import cx from "classnames"
+
+const data = {
+  name: "陳家衛",
+  jobTitle: "院長",
+  experience: ["采家醫學美容診所院長", "AAAMS 美國醫學美容外科協會講師", "采家醫學美容診所院長"],
+  expertise:
+    "各種雷射操作、皮秒雷射、微整形操作、音波拉提、埋線拉提、電波拉提、美白針、痘痘針、埋線拉提、電波拉提、美白針、痘痘針",
+}
 
 const DoctorCard = () => {
   const [isExperienceMore, setIsExperienceMore] = useState(false)
   const [isExpertiseMore, setIsExpertiseMore] = useState(false)
+  const [over, setOver] = useState<boolean>(false)
+  const ref = useRef<HTMLDivElement>(null)
+
+  useLayoutEffect(() => {
+    const clientHeight = ref.current?.clientHeight || 0
+    const scrollHeight = ref.current?.scrollHeight || 0
+    if (clientHeight && scrollHeight > clientHeight) {
+      setOver(true)
+    }
+  }, [])
+
   return (
     <div className={styled.doctor}>
       <div className={styled.personal}>
         <div className={styled.avatar} />
         <div className={styled.information}>
-          <div className={styled.name}>陳家衛</div>
-          <div className={styled["job-title"]}>院長</div>
+          <div className={styled.name}>{data.name}</div>
+          <div className={styled["job-title"]}>{data.jobTitle}</div>
         </div>
       </div>
-      <ul className={cx(styled.experience, { [styled.more]: isExperienceMore })}>
-        <li className={styled.job}>采家醫學美容診所院長</li>
-        <li className={styled.job}>AAAMS 美國醫學美容外科協會講師</li>
-        <li className={styled.job}>AAAMS 美國醫學美容外科協會講師</li>
+      <ul
+        className={cx(styled.experience, {
+          [styled.more]: isExperienceMore,
+        })}>
+        {data.experience.map(item => (
+          <li className={styled.job} key={item}>
+            {item}
+          </li>
+        ))}
       </ul>
-      <div className={styled.show} onClick={() => setIsExperienceMore(!isExperienceMore)}>
-        {isExperienceMore ? "顯示更少" : "顯示更多"}
+      {data.experience.length > 2 && (
+        <div className={styled.show} onClick={() => setIsExperienceMore(!isExperienceMore)}>
+          {isExperienceMore ? "顯示更少" : "顯示更多"}
+        </div>
+      )}
+      <div className={cx(styled.expertise, { [styled.more]: isExpertiseMore })} ref={ref}>
+        {data.expertise}
       </div>
-      <div className={cx(styled.expertise, { [styled.more]: isExpertiseMore })}>
-        各種雷射操作、皮秒雷射、微整形操作、音波拉提、埋線拉提、電波拉提、美白針、痘痘針
-      </div>
-      <div className={styled.show} onClick={() => setIsExpertiseMore(!isExpertiseMore)}>
-        {isExpertiseMore ? "顯示更少" : "顯示更多"}
-      </div>
+      {over && (
+        <div className={styled.show} onClick={() => setIsExpertiseMore(!isExpertiseMore)}>
+          {isExpertiseMore ? "顯示更少" : "顯示更多"}
+        </div>
+      )}
     </div>
   )
 }

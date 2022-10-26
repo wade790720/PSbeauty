b3e7bfcc9501ee0102a6419fadecb10ba8c974d7
wadezhu <wade790720@gmail.com>
2022-10-27 00:34:27 +0800

    Modify download

--- src/pages/Chatroom/ImageViewer/index.tsx
+++ src/pages/Chatroom/ImageViewer/index.tsx
@@ -1,6 +1,6 @@
 import cx from "classnames"
 import styled from "./ImageViewer.module.scss"
-import { saveAs } from "file-saver"
+// import { saveAs } from "file-saver"
 import { ReactComponent as Close } from "./Close.svg"
 import { ReactComponent as Download } from "./Download.svg"
 
@@ -19,17 +19,19 @@ const ImageViewer = ({ url, clickClose }: ImageViewerProps) => {
         }}>
         <Close />
       </div>
-      <div
+      <a
         className={cx(styled.btn, styled.right)}
-        onClick={async () => {
-          const res = await fetch(url)
-          const buffer = await res.arrayBuffer()
-          const array = url.replace(/\?.*$/, "").split("%2F")
-          const filename = array.length > 1 ? decodeURI(array[array.length - 1]) : "圖片.jpg"
-          saveAs(URL.createObjectURL(new Blob([buffer])), filename)
-        }}>
+        href={url}
+        // onClick={async () => {
+        //   const res = await fetch(url)
+        //   const buffer = await res.arrayBuffer()
+        //   const array = url.replace(/\?.*$/, "").split("%2F")
+        //   const filename = array.length > 1 ? decodeURI(array[array.length - 1]) : "圖片.jpg"
+        //   saveAs(URL.createObjectURL(new Blob([buffer])), filename)
+        // }}
+      >
         <Download />
-      </div>
+      </a>
       <img src={url} />
     </div>
   ) : (
